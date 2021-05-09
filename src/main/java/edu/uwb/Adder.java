b698cc78836b0caa6b5ab03decb2f1e4c97d8fe0
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-05-08 22:55:15 -0700

    Update Adder.java

--- src/main/java/edu/uwb/Adder.java
+++ src/main/java/edu/uwb/Adder.java
@@ -1,14 +1,12 @@
 package edu.uwb;
 
 public class Adder {
-    public int AddPositiveInts(int a, int b)
-    {
+    public int AddPositiveInts(int a, int b) {
         if ((a < 0) || (b < 0)) { return -1; }
         return a + b;
     }
     
-    public int Square(int a)
-    {
+    public int Square(int a) {
         return a * a;
     }
 }
