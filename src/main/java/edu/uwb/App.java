e4dea6c7202515d68cae909586f5e6857e87f6d8
Yusuf Pisan <pisan@uw.edu>
2021-05-08 22:01:27 -0700

    addding commit

--- src/main/java/edu/uwb/App.java
+++ src/main/java/edu/uwb/App.java
@@ -4,8 +4,7 @@ package edu.uwb;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
