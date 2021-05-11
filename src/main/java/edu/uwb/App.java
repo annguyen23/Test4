c94848c971971ebab26d90b137ec3c76aa550e9b
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-05-11 02:07:23 -0700

    Update App.java

--- src/main/java/edu/uwb/App.java
+++ src/main/java/edu/uwb/App.java
@@ -5,8 +5,7 @@ package edu.uwb;
  *
  */
 public class App {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
