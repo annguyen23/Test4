d53b65b03302b241dde0106ed45b7d054c0c6fbb
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-05-11 02:16:41 -0700

    Update Adder.java

--- src/main/java/edu/uwb/Adder.java
+++ src/main/java/edu/uwb/Adder.java
@@ -1,6 +1,6 @@
 package edu.uwb;
 
-public class Adder {
+public class Adder { 
     public int AddPositiveInts(int a, int b) {
         if ((a < 0) || (b < 0)) { return -1; }
         return a + b;
